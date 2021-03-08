f5fcc519ed373c71845cf9961b9a491a57d6ab77
JackLuu272 <78498034+JackLuu272@users.noreply.github.com>
2021-03-08 09:57:40 -0800

    Program is completed

--- problem3.py
+++ problem3.py
@@ -10,6 +10,6 @@ Example: The factors of 10 are 1, 2, 5, 10
 """
 import math
 def factors(a):
-    for i in range(0,a + 1):
+    for i in range(1,a + 1):
         if a % i == 0:
             return i 
