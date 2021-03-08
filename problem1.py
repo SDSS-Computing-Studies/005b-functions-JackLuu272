b0c8de5f6573557c1b8ba6e7f5c76609f7811ae5
JackLuu272 <78498034+JackLuu272@users.noreply.github.com>
2021-03-08 09:31:43 -0800

    Program is completed

--- problem1.py
+++ problem1.py
@@ -8,10 +8,10 @@ Return the missing side
 (2 points)
 """
 
-def hypotenuse(a,b):
-    if a**2 + b**2 == c**2:
+def hypotenuse(a,b,boolean):
+    if a**2 + b**2 == c**2 and boolean == True:
         return c 
-    elif a**2 + b**2 != c**2 and a > b:
+    elif a**2 + b**2 != c**2 and a > b and boolean == False:
         return a
     else:
         return b 
