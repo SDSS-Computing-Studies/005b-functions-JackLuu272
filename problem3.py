e36078b8848525ee03adc101288162e0bbb966ac
JackLuu272 <78498034+JackLuu272@users.noreply.github.com>
2021-03-08 10:15:33 -0800

    Program is completed

--- problem3.py
+++ problem3.py
@@ -9,7 +9,9 @@ Example: The factors of 10 are 1, 2, 5, 10
 (2 points)
 """
 import math
+list = []
 def factors(a):
     for i in range(1,a + 1):
         if a % i == 0:
-            return i 
+            list.append(i)
+            return(list)
