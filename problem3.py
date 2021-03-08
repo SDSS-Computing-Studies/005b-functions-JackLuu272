f494c70b405fc18541a640f8726c83fc0eeca337
JackLuu272 <78498034+JackLuu272@users.noreply.github.com>
2021-03-08 09:56:14 -0800

    Program is completed

--- problem3.py
+++ problem3.py
@@ -8,7 +8,8 @@ into another number.
 Example: The factors of 10 are 1, 2, 5, 10
 (2 points)
 """
+import math
 def factors(a):
-    for i in range(1,a + 1) and a > 0:
+    for i in range(0,a + 1):
         if a % i == 0:
-            return i
+            return i 
